53ce0f130b23554986f8de76a6c7301d40c0ea4d
vignesh5698 <vigneshit13@gmail.com>
2017-11-28 22:53:09 +0530

    Example for JSON parsing in Android

--- app/src/main/java/com/example/root/json1/MainActivity.java
+++ app/src/main/java/com/example/root/json1/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.root.json1;
 
 import android.app.ProgressDialog;
+import android.nfc.Tag;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ListView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,12 +40,37 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected void doInBackground(void... params) {
-            
+            HttpHandler sh=new HttpHandler();
+            String jsonstr=sh.makeServiceCall(url);
+            //Log.e(TAG,"Respose from URL");
+            if(jsonstr!=null){
+            try{
+                JSONObject jsonobj=new JSONObject(jsonstr);
+                JSONArray contacts=new jsonobj.getJSONArray("contacts");
+                for (int i=0;i<contacts.length();i++){
+                    JSONObject c=contacts.getJSONObject(i);
+                    String id=c.getString("id");
+                    String name=c.getString("name");
+                    String email=c.getString("email");
+                    String address=c.getString("address");
+                    String gender=c.getString("gender");
+                    JSONObject phone=c.getJSONObject("phone");
+                    String mobile=phone.getString("mobile");
+                    String home=phone.getString("home");
+                    String office=phone.getString("office");
+                    HashMap<String,String> contact=new HashMap<>();
+
+
+                }
+            }catch (Exception e){
+
+            }
+            }
+        }
         }
 
         @Override
         protected void onPostExecute(void aVoid) {
             super.onPostExecute(aVoid);
-        }
     }
 }
